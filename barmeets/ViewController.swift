5a444e9591ddcc1b04fe0c06e62cd0d6582eaa74
Artem Soshnikov <213036@skobka.com>
2024-05-18 11:47:20 +0500

    Initial Commit

---
+++ barmeets/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  barmeets
+//
+//  Created by Артем Сошников on 18.05.2024.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
